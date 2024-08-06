189ea283b755c1e1a2a27d13a7f8f24d66066688
yang404-1 <yjl0130@apps.ntpc.edu.tw>
2024-08-06 19:39:29 +0800

    Create ex4-1.py

---
+++ ex4-1.py
@@ -0,0 +1,27 @@
+"""
+x = "Hello World"
+x = 20	
+x = 20.5	
+x = 1j	
+x = ["apple", "banana", "cherry"]	
+"""
+z = ("apple", "banana", "cherry")	
+y = range(6)
+x = {"name" : "John", "age" : 36}
+z1 = {"apple", "banana", "cherry"}
+"""
+x = frozenset({"apple", "banana", "cherry"})	
+x = True	
+x = b"Hello"	
+x = bytearray(5)	
+x = memoryview(bytes(5))
+x = None
+"""
+print(x)
+print(type(x))
+print(y)
+print(type(y))
+print(z)
+print(type(z))
+print(z1)
+print(type(z1))
