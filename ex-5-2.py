5a57602b6cb8bbd41b9a85f8bca09dae83c1ee04
yang404-1 <yjl0130@apps.ntpc.edu.tw>
2024-08-13 19:34:42 +0800

    Create ex-5-2.py

---
+++ ex-5-2.py
@@ -0,0 +1,13 @@
+y=0.514
+x=114
+
+print(x+y*x/x-y**y%x %y+y+y+y+y)
+print(x+y*x*x-x-x*y%x+y+y+y+y*y*y*y**y*y*y*y*y*y*y*y**y*y*y*y*y*y*y*y*y*y*y*y*y*y**y*y*y**y*y*y*y*y**y*y*y*y*y*y*y*y*y*y*y*y*y*y*y*y*y*y*y*y)
+print(x+y)
+print(x-y)
+print(x*y)
+print(x>y*y*y*y*y*y**y*y*y*y*y*y*y*y*x/y/y/y/y/y/y/y//y/y/y/y/y/y/y/x)
+print(x>y)
+print(x<y or x>114514)
+print(x==y and x>114514)
+print(not(x > 3 and x < 114514))
