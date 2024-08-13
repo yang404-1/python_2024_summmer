9a42cbe40353a2186cd17324e68a68410f9e4fff
yang404-1 <yjl0130@apps.ntpc.edu.tw>
2024-08-13 19:56:55 +0800

    Create ex5-5-1.py

---
+++ ex5-5-1.py
@@ -0,0 +1,24 @@
+y=0.514
+x=1
+
+print(x+y*x/x-y**y%x %y+y+y+y+y)
+print(x+y*x*x-x-x*y%x+y+y+y+y*y*y*y**y*y*y*y*y*y*y*y**y*y*y*y*y*y*y*y*y*y*y*y*y*y**y*y*y**y*y*y*y*y**y*y*y*y*y*y*y*y*y*y*y*y*y*y*y*y*y*y*y*y)
+print(x+y)
+print(x-y)
+print(x*y)
+print(x>y*y*y*y*y*y**y*y*y*y*y*y*y*y*x/y/y/y/y/y/y/y//y/y/y/y/y/y/y/x)
+print(x>y)
+print(x<y or x>114514)
+print(x==y and x>114514)
+print(not(x > 3 and x < 114514))
+if x+y ==114.514:
+    print("114514首")
+if y > x:
+  print("x is greater than y")
+elif x == y:
+  print("x and y are equal")
+else:
+  print("x is greater than y")
+while(x<10):
+    print(x)
+    x=x+1
