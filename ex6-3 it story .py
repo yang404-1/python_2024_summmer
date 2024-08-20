dafacaeba4af341cf52b82feef4f27780eea5508
yang404-1 <yjl0130@apps.ntpc.edu.tw>
2024-08-20 19:41:25 +0800

    Create ex6-3 it story .py

---
+++ ex6-3 it story .py
@@ -0,0 +1,44 @@
+'''
+
+it a story 
+
+'''
+def my_function():
+  print("not")
+
+my_function()
+def my_function():
+  print("not anything")
+
+my_function()
+def my_function():
+  print("just nothing")
+
+my_function()
+def my_function():
+  print("don't look here is nothing")
+
+my_function()
+def my_function():
+  print("you want look here have what. you can see ok?")
+
+my_function()
+def my_function():
+  print("ok let me find....")
+
+my_function()
+
+def my_function():
+  print(" ")
+
+my_function()
+
+def my_function():
+  print(" ")
+
+my_function()
+
+def my_function():
+  print("ooh! i have a cool thing it a................ python3!")
+
+my_function()
