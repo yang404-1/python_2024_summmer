4d07d38de9eb826e78a159fd77bfd4d880cfec32
yang404-1 <yjl0130@apps.ntpc.edu.tw>
2024-08-20 19:23:46 +0800

    Create ex6-1.py

---
+++ ex6-1.py
@@ -0,0 +1,20 @@
+x=("----------------------------------")
+r = -1
+while r > -101:
+  print(r)
+  r -= 1
+  i = 100
+while i > 0:
+  print(i)
+  i -= 1
+  if i == 100:
+      break
+  print(x)
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break
+  print(x)
+  print(i)
+  print(r)
