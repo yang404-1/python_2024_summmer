2f196faf8872b35e9193bce05ee8863a2df48e06
yang404-1 <yjl0130@apps.ntpc.edu.tw>
2024-08-20 19:48:38 +0800

    Create ex6-3.py

---
+++ ex6-3.py
@@ -0,0 +1,55 @@
+'''
+
+it a story 
+
+'''
+def my_function():
+  print("======= you look arond but here is nothing =======")
+
+my_function()
+def my_function():
+  print("yes it's nothing here")
+
+my_function()
+def my_function():
+  print("just nothing")
+
+my_function()
+
+my_function()
+def my_function():
+  print("just nothing")
+
+my_function()
+def my_function():
+  print("yap it's nothing")
+
+my_function()
+def my_function():
+  print("you look here is nothing?")
+
+my_function()
+def my_function():
+  print("you want something here? ")
+
+my_function()
+def my_function():
+  print("ok. let me find....")
+
+my_function()
+
+def my_function():
+  print(" ")
+
+my_function()
+
+def my_function():
+  print(" ")
+
+my_function()
+
+def my_function():
+  print(" cool thing is ................ python3!")
+  
+
+my_function()
